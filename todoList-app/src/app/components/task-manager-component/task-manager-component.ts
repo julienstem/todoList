664793cfd26d9d113c846660136cfec07f82b947
julienstem <stempferjulien@gmail.com>
2025-11-28 15:23:48 +0100

    init task manager component

---
+++ todoList-app/src/app/components/task-manager-component/task-manager-component.ts
@@ -0,0 +1,11 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-task-manager-component',
+  imports: [],
+  templateUrl: './task-manager-component.html',
+  styleUrl: './task-manager-component.css',
+})
+export class TaskManagerComponent {
+
+}
