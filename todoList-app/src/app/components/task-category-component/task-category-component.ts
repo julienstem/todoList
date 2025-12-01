e9ae0dc3a6ce6ae3b061b444c1ce07482a0b20fd
julienstem <stempferjulien@gmail.com>
2025-12-01 09:17:17 +0100

    create category component and service

---
+++ todoList-app/src/app/components/task-category-component/task-category-component.ts
@@ -0,0 +1,11 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-task-category-component',
+  imports: [],
+  templateUrl: './task-category-component.html',
+  styleUrl: './task-category-component.css',
+})
+export class TaskCategoryComponent {
+
+}
